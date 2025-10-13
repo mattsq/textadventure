c9a40ade51130d37e56fb8cc86cb3b028caa58c2
Matthew Simmons <mattsq@users.noreply.github.com>
2025-10-13 21:52:20 +1100

    Fix Vite env typings for typecheck

--- web/scene-editor/src/env.d.ts
+++ web/scene-editor/src/env.d.ts
@@ -1,8 +1,6 @@
+/// <reference types="vite/client" />
+
 interface ImportMetaEnv {
   readonly VITE_SCENE_API_BASE_URL?: string;
-  readonly [key: string]: string | undefined;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly [key: string]: string | boolean | undefined;
 }
